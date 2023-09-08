a783e4de9ee1a2079fa818223e345fd8c3ac59ae
Alexandre Salconi <asalconi@cimeq.qc.ca>
2023-09-08 12:53:01 -0400

    change the fuzzy logic and reformat verbose

--- labelMigrator.py
+++ labelMigrator.py
@@ -2,6 +2,7 @@ import os
 import giteapy
 from pprint import pprint
 from thefuzz import fuzz
+from thefuzz import process
 
 testOrg = "testOrg"
 
@@ -26,35 +27,44 @@ class LabelMigrator:
         return api_response
 
     def process(self):
-        api_response = self.__get_all_org()
-        api_response.remove(self.ref_organisation)
-        pprint(api_response)
+        list_of_organisation = self.__get_all_org()
+        list_of_organisation.remove(self.ref_organisation)
+
+        for target_organisation in list_of_organisation:
+            self.__process_organisation(target_organisation)
         # Next we need to foreach all org and compare with specific label reference
 
-    def __find_label_in_pool(self, reference_label, target_labels) -> bool:
-        for target_label in target_labels:
-            accuracy = fuzz.partial_ratio(str(reference_label.name), str(target_label.name))
-            if accuracy > 70:
-                # print("match({}) {} ~= {}".format(accuracy, reference_label.name, target_label.name))
-                return True
-        # print("no match for {}".format(reference_label.name))
-        return False
+    def __process_organisation(self, target_organisation):
+        label_from_source = self.__get_ref_label(self.ref_organisation)
+        label_from_target = self.__get_ref_label(testOrg)
 
-    def test(self):
-        labelFromSource = self.__get_ref_label(self.ref_organisation)
-        labelFromTarget = self.__get_ref_label(testOrg)
-
-        for referenceLabel in labelFromSource:
+        for referenceLabel in label_from_source:
             referenceLabel.id = 0  # use id to mark when it's added
 
-        for referenceLabel in labelFromSource:
-            if self.__find_label_in_pool(referenceLabel, labelFromTarget):
+        for referenceLabel in label_from_source:
+            result = self.__find_label_in_pool(referenceLabel, label_from_target)
+            if result:
                 referenceLabel.id = 1  # use id to mark when it's added
-                # print(referenceLabel.name)
+                print("UPDATE: {} -> {} ".format(result, referenceLabel.name))
 
-        for referenceLabel in labelFromSource:
-            if referenceLabel.id == 1:
-                print("match {}".format(referenceLabel.name))
-            else:
-                print("to create {}".format(referenceLabel.name))
+        for referenceLabel in label_from_source:
+            if referenceLabel.id != 1:
+                print("CREATE: {}".format(referenceLabel.name))
+
+    def __find_label_in_pool(self, reference_label, target_labels) -> str | None:
+        list_label_from_target = list()
+        for target_label in target_labels:
+            list_label_from_target.append(target_label.name)
+        result = process.extract(reference_label.name, list_label_from_target, limit=1)[0]
+
+        accuracy = result[1]
+        if accuracy > 70:
+            # print("match({}) {} ~= {}".format(accuracy, reference_label.name, result[0]))
+            return result[0]
+        # print("no match for {}".format(reference_label.name))
+        return None
+
+    def test(self):
+        self.__process_organisation(testOrg)
+
         # pprint(labelFromSource)
