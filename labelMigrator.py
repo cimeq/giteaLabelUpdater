b9623c33831d5ac0ee672f0346d71276e56cb400
Alexandre Salconi <asalconi@cimeq.qc.ca>
2023-09-08 11:47:51 -0400

    update function to list organisation

--- labelMigrator.py
+++ labelMigrator.py
@@ -1,5 +1,6 @@
 import os
 import giteapy
+from pprint import pprint
 
 
 class LabelMigrator:
@@ -9,7 +10,21 @@ class LabelMigrator:
         self.configuration.api_key['access_token'] = git_token
         self.configuration.host = host_api
 
+    def __get_all_org(self) -> list:
+        result = list()
+        api_instance = giteapy.OrganizationApi(giteapy.ApiClient(self.configuration))
+        api_response = api_instance.org_get_all()
+        for response in api_response:
+            result.append(response.name)
+        return result
+
+    def __get_ref_label(self, organisation: str):
+        api_instance = giteapy.OrganizationApi(giteapy.ApiClient(self.configuration))
+        api_response = api_instance.org_list_labels(organisation)
+        return api_response
+
     def test(self):
-        api_instance = giteapy.MiscellaneousApi(giteapy.ApiClient(self.configuration))
-        api_response = api_instance.get_version()
-        print(api_response)
+
+        api_response = self.__get_all_org()
+        api_response.remove(self.ref_organisation)
+        pprint(api_response)
